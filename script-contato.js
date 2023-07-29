edd3d91b2975f39d245fd4ee0da7fa82e90fca81
davileal7 <108898208+davileal7@users.noreply.github.com>
2023-07-28 22:00:35 -0300

    script-contato 2

--- script-contato.js
+++ script-contato.js
@@ -1,13 +1,13 @@
-let btn = document.querySelector(".mobile-menu-icon")
-let menuMobile = document.querySelector(".mobile-menu")
+let btn = document.querySelector(".mobile-menu-icon2")
+let menuMobile = document.querySelector(".mobile-menu2")
 
 
 btn.addEventListener('click', ()=> {
-   if (menuMobile.classList.contains('open')) {
-        menuMobile.classList.remove('open')
+   if (menuMobile.classList.contains('open2')) {
+        menuMobile.classList.remove('open2')
         document.querySelector(".icon").src = 'menu_icon/menu_white_36dp.svg'
    } else {
-    menuMobile.classList.add('open')
+    menuMobile.classList.add('open2')
     document.querySelector(".icon").src = 'menu_icon/close_white_36dp.svg'
    }  
 })
